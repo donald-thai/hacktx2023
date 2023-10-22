e0e5a97b0276e06395cddebdd524fa93f94a0b0c
AndrewFortner <fortnerand@gmail.com>
2023-10-22 12:04:10 -0500

    new prompt

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS
 import openai
-openai.api_key = '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'
+openai.api_key = 'YOUR_API_KEY'
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,12 +23,12 @@ class CodexComplete(Resource):
     
 
     def complete_with_codex(self, code):
-        response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{'role': 'user', 'content': "Complete the code that ends on this line. Respond only with the code that follows this and nothing more. Only autocomplete 1 line:\n" + code}],
+        response = openai.Completion.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt="Complete the code that ends on this line. Respond only with the code that follows this and nothing more. Only autocomplete 1 line:\n" + code,
         )
         # Take the original length and the next 10 words
-        response = response.choices[0].message['content']
+        response = response.choices[0].text
         return response
 
 
